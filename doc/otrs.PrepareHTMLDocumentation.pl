6d588f07b798f6a17782cd890ef93561be68e6cc
Marc A. Bonsels <marc.bonsels@otrs.com>
2014-03-28 11:23:45 +0100

    Added jquery; updated CSS.

--- doc/otrs.PrepareHTMLDocumentation.pl
+++ doc/otrs.PrepareHTMLDocumentation.pl
@@ -146,6 +146,7 @@ $OriginalBodyContent
         </div>
     </div>
 </div>
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="../../../../doku.js"></script>
 </body>
 EOF
