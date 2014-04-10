debc1c2b1977b0fbd38030b7970828b04d05d3ea
Marc A. Bonsels <marc.bonsels@otrs.com>
2014-04-10 11:58:32 +0200

    Updated github start page.

--- doc/documentation.js
+++ doc/documentation.js
@@ -229,7 +229,7 @@ $(document).ready(function() {
         $('body').addClass('manual');
 
         // Make table of contents collapsable
-        $('.toc p b a').on('click', function() {
+        $('.toc').on('click', 'a', function() {
             $(this).parent().parent().next('dl').slideToggle('fast', function() {
                 $(this).parent().toggleClass('closed');
             });
