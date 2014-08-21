f76bf7e5ab65652fcb6031d5d4d6854460bb302c
Martin Gruner <martin.gruner@otrs.com>
2014-08-21 13:58:10 +0200

    Use protocol-relative URLs to properly support HTTPS.

--- doc/PrepareHTMLDocumentation.pl
+++ doc/PrepareHTMLDocumentation.pl
@@ -101,10 +101,10 @@ sub ProcessHTMLFile {
 
     my $HTMLInject =<<"EOF";
 <!-- otrs.github.io -->
-<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link href='//fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 <link rel="stylesheet" href="$PathToJS/documentation.css">
-<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript" src="$PathToJS/documentation.js"></script>
 <!-- otrs.github.io -->
 EOF
