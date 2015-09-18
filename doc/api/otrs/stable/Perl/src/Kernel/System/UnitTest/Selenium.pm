4f8a8727be96c8e35d715b28f130efc56f426ef6
otrsbot <martin.gruner+otrsbot@otrs.com>
2015-09-18 06:21:19 +0200

    Updated generated documentation (api 4.0).

--- doc/api/otrs/stable/Perl/src/Kernel/System/UnitTest/Selenium.pm
+++ doc/api/otrs/stable/Perl/src/Kernel/System/UnitTest/Selenium.pm
@@ -13,7 +13,6 @@ package Kernel::System::UnitTest::Selenium;
 use strict;
 use warnings;
 
-use base qw(Selenium::Remote::Driver);
 use MIME::Base64();
 use File::Temp();
 
@@ -83,6 +82,9 @@ sub new {
             die "SeleniumTestsConfig must provide $Needed!";
         }
     }
+
+    $Kernel::OM->Get('Kernel::System::Main')->RequireBaseClass('Selenium::Remote::Driver')
+        || die "Could not load Selenium::Remote::Driver";
 
     my $Self = $Class->SUPER::new(%SeleniumTestsConfig);
     $Self->{UnitTestObject}      = $Param{UnitTestObject};
@@ -250,7 +252,7 @@ sub Login {
         # Wait until form has loaded, if neccessary
         ACTIVESLEEP:
         for my $Second ( 1 .. 20 ) {
-            if ( $Self->execute_script("return \$('a#LogoutButton').length") ) {
+            if ( $Self->execute_script('return typeof($) === "function" && $("a#LogoutButton").length') ) {
                 last ACTIVESLEEP;
             }
             sleep 1;
