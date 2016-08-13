086c5a0370ace75c6ab41956690d901d26c824b7
otrsbot <martin.gruner+otrsbot@otrs.com>
2016-08-13 06:30:49 +0200

    Updated generated documentation (api 5.0).

--- doc/api/otrs/5.0/Perl/src/Kernel/System/Ticket/TicketSearch.pm
+++ doc/api/otrs/5.0/Perl/src/Kernel/System/Ticket/TicketSearch.pm
@@ -2137,6 +2137,25 @@ sub TicketSearch {
                 $SQLSelect .= ", $SQLOrderField ";
                 $SQLExt    .= " $SQLOrderField ";
             }
+            elsif (
+                $SortByArray[$Count] eq 'Owner'
+                || $SortByArray[$Count] eq 'Responsible'
+                )
+            {
+                # include first and last name in select
+                $SQLSelect
+                    .= ', ' . $SortOptions{ $SortByArray[$Count] }
+                    . ", u.first_name, u.last_name ";
+
+                # join the users table on user's id
+                $SQLFrom
+                    .= ' JOIN users u '
+                    . ' ON ' . $SortOptions{ $SortByArray[$Count] } . ' = u.id ';
+
+                # sort by first and last name
+                my $OrderBySuffix = $OrderByArray[$Count] eq 'Up' ? 'ASC' : 'DESC';
+                $SQLExt .= " u.first_name $OrderBySuffix, u.last_name ";
+            }
             else {
 
                 # regular sort
