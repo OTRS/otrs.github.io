8adfc52b9f9d40c86ac9425902678f17bfc4b7f7
otrsbot <martin.gruner+otrsbot@otrs.com>
2016-09-20 06:00:38 +0200

    Updated generated documentation (api 6.0).

--- doc/api/otrs/6.0/Perl/src/Kernel/Config/Defaults.pm
+++ doc/api/otrs/6.0/Perl/src/Kernel/Config/Defaults.pm
@@ -17,8 +17,8 @@ use strict;
 use warnings;
 use utf8;
 
-# Perl 5.10.0 is the required minimum version to use OTRS.
-use 5.010_000;
+# Perl 5.16.0 is the required minimum version to use OTRS.
+use 5.016_000;
 
 # prepend '../Custom', '../Kernel/cpan-lib' and '../' to the module search path @INC
 use File::Basename;
@@ -1518,6 +1518,8 @@ via the Preferences button after logging in.
             [ 'UserCountry',      Translatable('Country'),     'country',      1, 0, 'var', '', 0 ],
             [ 'UserComment',      Translatable('Comment'),     'comments',     1, 0, 'var', '', 0 ],
             [ 'ValidID',          Translatable('Valid'),       'valid_id',     0, 1, 'int', '', 0 ],
+            # this is needed, if "SMIME::FetchFromCustomer" is active
+            #[ 'SMIMECertificate', 'SMIMECertificate', 'userSMIMECertificate',      0, 1, 'var', '', 0 ],
 
             # Dynamic field example
 #            [ 'DynamicField_Name_X', undef, 'Name_X', 0, 0, 'dynamic_field', undef, 0, undef, undef, ],
