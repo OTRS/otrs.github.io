d37fd2265f11b43b1e2ca8d3ae31776bb331de5f
otrsbot <martin.gruner+otrsbot@otrs.com>
2017-09-09 04:35:13 +0200

    Updated generated documentation (api 5.0).

--- doc/api/otrs/stable/Perl/src/Kernel/System/Stats.pm
+++ doc/api/otrs/stable/Perl/src/Kernel/System/Stats.pm
@@ -1199,11 +1199,11 @@ sub ObjectModuleCheck {
     return if $Param{StatType} ne 'static' && $Param{StatType} ne 'dynamic';
 
     my $CheckFileLocation = 'Kernel::System::Stats::' . ucfirst $Param{StatType};
-    return if $Param{ObjectModule} !~ m{ \A $CheckFileLocation }xms;
+    my $CheckPackageName  = '[A-Z_a-z][0-9A-Z_a-z]*';
+    return if $Param{ObjectModule} !~ m{ \A $CheckFileLocation (?: ::$CheckPackageName)+ \z }xms;
 
     my $ObjectName = [ split( m{::}, $Param{ObjectModule} ) ]->[-1];
     return if !$ObjectName;
-    return if $ObjectName !~ m{[A-Z_a-z][0-9A-Z_a-z]*}xms;
 
     my @RequiredObjectFunctions;
 
@@ -3760,6 +3760,10 @@ sub _MonthArray {
 
 sub _AutomaticSampleImport {
     my ( $Self, %Param ) = @_;
+
+    # Prevent deep recursions.
+    local $Self->{InAutomaticSampleImport} = $Self->{InAutomaticSampleImport};
+    return if $Self->{InAutomaticSampleImport}++;
 
     for my $Needed (qw(UserID)) {
         if ( !$Param{$Needed} ) {
