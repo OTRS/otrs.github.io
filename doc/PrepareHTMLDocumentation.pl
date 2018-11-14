4d2219668523d1e82c57778c731386a450f3f57a
Martin Gruner <martin.gruner@otrs.com>
2018-11-14 09:59:06 +0100

    Make exclude for the new RST docs more generic.

--- doc/PrepareHTMLDocumentation.pl
+++ doc/PrepareHTMLDocumentation.pl
@@ -57,7 +57,7 @@ sub FindHTMLFiles {
         return if (substr($File::Find::name, -5) ne '.html');
 
         # Don't modify the new RST documentations - not compatible.
-        return if ($File::Find::name =~ m{/admin-beta/});
+        return if ($File::Find::name =~ m{/manual/.*/7});
 
         # Don't modify the REST API documentation at this point - not compatible.
         return if ($File::Find::name =~ m{/REST/});
