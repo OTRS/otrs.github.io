5b434a0e3be2da73d3522fc8a28823f66ff4f4b0
otrsbot <martin.gruner+otrsbot@otrs.com>
2019-01-25 04:25:52 +0000

    Updated generated documentation (api 8.0).

--- doc/api/otrs/8.0/frontend/dist/api/script/search_index.js
+++ doc/api/otrs/8.0/frontend/dist/api/script/search_index.js
@@ -6,6 +6,12 @@ window.esdocSearchIndex = [
     "class"
   ],
   [
+    "announcer~announcer",
+    "class/Frontend/Announcer/index.js~Announcer.html",
+    "<span>Announcer</span> <span class=\"search-result-import-path\">Announcer</span>",
+    "class"
+  ],
+  [
     "form/validator/datatype~datatype",
     "class/Frontend/Form/Validator/DataType.js~DataType.html",
     "<span>DataType</span> <span class=\"search-result-import-path\">Form/Validator/DataType</span>",
@@ -63,6 +69,12 @@ window.esdocSearchIndex = [
     "server/perlprofiler~perlprofiler",
     "class/Frontend/Server/PerlProfiler/index.js~PerlProfiler.html",
     "<span>PerlProfiler</span> <span class=\"search-result-import-path\">Server/PerlProfiler</span>",
+    "class"
+  ],
+  [
+    "plugins/announcer~plugin",
+    "class/Frontend/Plugins/Announcer/index.js~Plugin.html",
+    "<span>Plugin</span> <span class=\"search-result-import-path\">Plugins/Announcer</span>",
     "class"
   ],
   [
@@ -756,6 +768,30 @@ window.esdocSearchIndex = [
     "external"
   ],
   [
+    "frontend/announcer/index.js",
+    "file/Frontend/Announcer/index.js.html",
+    "Frontend/Announcer/index.js",
+    "file"
+  ],
+  [
+    "frontend/announcer/index.js~announcer#$bus",
+    "class/Frontend/Announcer/index.js~Announcer.html#instance-member-$bus",
+    "Frontend/Announcer/index.js~Announcer#$bus",
+    "member"
+  ],
+  [
+    "frontend/announcer/index.js~announcer#constructor",
+    "class/Frontend/Announcer/index.js~Announcer.html#instance-constructor-constructor",
+    "Frontend/Announcer/index.js~Announcer#constructor",
+    "method"
+  ],
+  [
+    "frontend/announcer/index.js~announcer#setmessage",
+    "class/Frontend/Announcer/index.js~Announcer.html#instance-method-setMessage",
+    "Frontend/Announcer/index.js~Announcer#setMessage",
+    "method"
+  ],
+  [
     "frontend/apps/agent/helpers/loginpostauthregistration.js",
     "file/Frontend/Apps/Agent/Helpers/LoginPostAuthRegistration.js.html",
     "Frontend/Apps/Agent/Helpers/LoginPostAuthRegistration.js",
@@ -1266,6 +1302,18 @@ window.esdocSearchIndex = [
     "file"
   ],
   [
+    "frontend/plugins/announcer/index.js",
+    "file/Frontend/Plugins/Announcer/index.js.html",
+    "Frontend/Plugins/Announcer/index.js",
+    "file"
+  ],
+  [
+    "frontend/plugins/announcer/index.js~plugin.install",
+    "class/Frontend/Plugins/Announcer/index.js~Plugin.html#static-method-install",
+    "Frontend/Plugins/Announcer/index.js~Plugin.install",
+    "method"
+  ],
+  [
     "frontend/plugins/eventbus/index.js",
     "file/Frontend/Plugins/EventBus/index.js.html",
     "Frontend/Plugins/EventBus/index.js",
@@ -1495,13 +1543,13 @@ window.esdocSearchIndex = [
   ],
   [
     "frontend/server/apiclient/index.js~apiclient#options",
-    "class/Frontend/Server/APIClient/index.js~APIClient.html#instance-get-options",
+    "class/Frontend/Server/APIClient/index.js~APIClient.html#instance-set-options",
     "Frontend/Server/APIClient/index.js~APIClient#options",
     "member"
   ],
   [
     "frontend/server/apiclient/index.js~apiclient#options",
-    "class/Frontend/Server/APIClient/index.js~APIClient.html#instance-set-options",
+    "class/Frontend/Server/APIClient/index.js~APIClient.html#instance-get-options",
     "Frontend/Server/APIClient/index.js~APIClient#options",
     "member"
   ],
@@ -1587,6 +1635,12 @@ window.esdocSearchIndex = [
     "frontend/styles/globals.js",
     "file/Frontend/Styles/globals.js.html",
     "Frontend/Styles/globals.js",
+    "file"
+  ],
+  [
+    "frontend/tests/announcer.js",
+    "file/Frontend/Tests/Announcer.js.html",
+    "Frontend/Tests/Announcer.js",
     "file"
   ],
   [
@@ -1767,6 +1821,12 @@ window.esdocSearchIndex = [
     "frontend/tests/components/form/field/forminput.js",
     "file/Frontend/Tests/Components/Form/Field/FormInput.js.html",
     "Frontend/Tests/Components/Form/Field/FormInput.js",
+    "file"
+  ],
+  [
+    "frontend/tests/components/form/field/formlanguage.js",
+    "file/Frontend/Tests/Components/Form/Field/FormLanguage.js.html",
+    "Frontend/Tests/Components/Form/Field/FormLanguage.js",
     "file"
   ],
   [
--- doc/api/otrs/8.0/frontend/dist/designsystem/94448e2a88f067bb4e13/js/141.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[141],{XJnz:function(n,w,o){}}]);
