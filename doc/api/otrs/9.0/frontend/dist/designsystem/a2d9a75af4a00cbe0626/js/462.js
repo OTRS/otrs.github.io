d7b4c376c650edd46aef00aaeb567c9defd27345
otrsbot <martin.gruner+otrsbot@otrs.com>
2020-04-23 04:26:10 +0000

    Updated generated documentation (api 9.0).

--- doc/api/otrs/9.0/frontend/dist/designsystem/a2d9a75af4a00cbe0626/js/462.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[462],{oxjK:function(o,n,p){o.exports=p.p+"fonts/streamline-regular.274b56b.woff"}}]);
--- doc/api/otrs/9.0/frontend/dist/designsystem/a2d9a75af4a00cbe0626/js/463.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[463],{bGLB:function(n,o,p){n.exports=p.p+"fonts/streamline-regular.4c475d1.woff2"}}]);
